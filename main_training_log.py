60c54f02fbcb41d6f8609b45f652d901554df490
Amadeus <amsl@gmx.at>
2020-05-17 09:19:33 +0200

    fix training log

--- main_training_log.py
+++ main_training_log.py
@@ -6,9 +6,5 @@ from modules.training_log import TrainingLog
 from config import ROOT_LIST, PLOT_PATH
 import os
 
-ROOT_LIST =["/Users/amadeus/Documents/garmin/tracks", '/Users/amadeus/Documents/Movescount/2015/']
-
-training_log = TrainingLog(2015, ROOT_LIST)
-# efforts = training_log.effort_list
-
+training_log = TrainingLog(2020, ROOT_LIST)
 training_log.plot_calendar(os.path.join(PLOT_PATH, 'training_log_{}.pdf'.format(training_log.year)))
